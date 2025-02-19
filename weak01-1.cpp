288df748fefc0a659a383bfbf654d2b3f0a9f225
Ting1013 <13750830@me.mcu.edu.tw>
2025-02-19 11:55:54 +0800

    weak01

---
+++ weak01-1.cpp
@@ -0,0 +1,14 @@
+///week01-1.cpp 瘋狂程設 兩數相加
+
+
+#include<stdio.h>
+///stdio的意思是，標準的 standard  intput/output
+///有stdio.h的外掛，才能用scanf() print()
+int main()
+{
+    int a, b; ///整a,b
+    scanf("%d",  &a );///掃描用
+    scanf("%d",  &b );///
+
+    printf("%d",  a+b );///印東西
+    }
