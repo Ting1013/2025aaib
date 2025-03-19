8414ff1b1681b8af0dc1efea4a33270f0d8f5bf3
Ting1013 <13750830@me.mcu.edu.tw>
2025-03-19 11:28:52 +0800

    Week05

---
+++ Week05-3.cpp
@@ -0,0 +1,19 @@
+///Week05-3.cpp
+///LeetCode 2206. Divide Array Into Equal Pairs
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int a[501] = {};//
+        int odd = 0;
+        for(int i=0; i<nums.size();i++){
+            int now = nums[i];
+            a[now]++;
+            if(a[now] % 2 == 0) odd--;
+            else odd++;
+        }
+        if(odd==0)return true;
+        else return false;
+
+    }
+};
